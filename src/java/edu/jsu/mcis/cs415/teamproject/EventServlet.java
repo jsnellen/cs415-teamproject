48bd4c2f17688cdddedef5661f1dbb6a5e773a99
Jayden Duncan <jduncan9@stu.jsu.edu>
2022-11-10 13:00:40 -0600

    EventServlet doPost(), doPut(), and doDelete() implemented

--- src/java/edu/jsu/mcis/cs415/teamproject/EventServlet.java
+++ src/java/edu/jsu/mcis/cs415/teamproject/EventServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import edu.jsu.mcis.cs415.teamproject.dao.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import javax.servlet.ServletContext;
 
 public class EventServlet extends HttpServlet {
@@ -46,6 +50,156 @@ public class EventServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        DAOFactory daoFactory = null;
+        
+        ServletContext context = request.getServletContext();
+        
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating new DAOFactory ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        response.setContentType("application/json; charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            
+            EventDAO e_dao = daoFactory.getEventDAO();
+            HashMap<String, String> params = new HashMap<>();
+            
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            
+            // get parameters from client
+            String description = request.getParameter("description");
+            String minute = request.getParameter("minute");
+            String hour = request.getParameter("hour");
+            String day_of_month = request.getParameter("day_of_month");
+            String month_of_year = request.getParameter("month_of_year");
+            String day_of_week = request.getParameter("day_of_week");
+            String year = request.getParameter("year");
+            Long duration  = Long.parseLong(request.getParameter("duration"));
+            ZonedDateTime active = ZonedDateTime.of(2022, 11, 1, 0, 0, 0, 0, ZoneId.of("UTC")); // default
+            ZonedDateTime inactive = ZonedDateTime.of(2022, 11, 30, 23, 59, 59, 0, ZoneId.of("UTC")); // default
+            
+            // fill HashMap with parameters
+            params.put("description", description);
+            params.put("minute", minute);
+            params.put("hour", hour);
+            params.put("day_of_month", day_of_month);
+            params.put("month_of_year", month_of_year);
+            params.put("day_of_week", day_of_week);
+            params.put("year", year);
+            params.put("duration", Long.toString(duration));
+            params.put("utc_active", dtf.format(active));
+            params.put("utc_inactive", dtf.format(inactive));
+            
+            // fill Event object
+            Event e1 = new Event(params);
+            
+            // call create() EventDAO method
+            out.println(e_dao.create(e1));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        
+        DAOFactory daoFactory = null;
+        
+        ServletContext context = request.getServletContext();
+        
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating new DAOFactory ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        response.setContentType("application/json; charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            
+            EventDAO e_dao = daoFactory.getEventDAO();
+            HashMap<String, String> params = new HashMap<>();
+            
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            
+            // get parameters from client
+            int id = Integer.parseInt(request.getParameter("id"));
+            String description = request.getParameter("description");
+            String minute = request.getParameter("minute");
+            String hour = request.getParameter("hour");
+            String day_of_month = request.getParameter("day_of_month");
+            String month_of_year = request.getParameter("month_of_year");
+            String day_of_week = request.getParameter("day_of_week");
+            String year = request.getParameter("year");
+            Long duration  = Long.parseLong(request.getParameter("duration"));
+            ZonedDateTime active = ZonedDateTime.of(2022, 11, 1, 0, 0, 0, 0, ZoneId.of("UTC")); // default
+            ZonedDateTime inactive = ZonedDateTime.of(2022, 11, 30, 23, 59, 59, 0, ZoneId.of("UTC")); // default
+            
+            // find event
+            Event e = e_dao.find(id);
+            
+            // update event attributes
+            e.setDescription(description);
+            e.setMinute(minute);
+            e.setHour(hour);
+            e.setDayOfMonth(day_of_month);
+            e.setMonthOfYear(month_of_year);
+            e.setDayOfWeek(day_of_week);
+            e.setYear(year);
+            e.setDuration(duration);
+            e.setUtcActive(active);
+            e.setUtcInactive(inactive);
+            
+            // call EventDAO update method
+            out.println(e_dao.update(e));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        
+        DAOFactory daoFactory = null;
+        
+        ServletContext context = request.getServletContext();
+        
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating new DAOFactory ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        response.setContentType("application/json; charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            
+            EventDAO e_dao = daoFactory.getEventDAO();
+            out.println(e_dao.delete(id));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 
    
