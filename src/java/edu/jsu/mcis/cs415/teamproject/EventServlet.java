c84d559109dd436579b8f21a40227e545346165d
Favour Ejemole <ejemolefavour@gmail.com>
2022-11-09 19:32:48 -0600

    doGet for EventServlet implemented

--- src/java/edu/jsu/mcis/cs415/teamproject/EventServlet.java
+++ src/java/edu/jsu/mcis/cs415/teamproject/EventServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import edu.jsu.mcis.cs415.teamproject.dao.*;
+import javax.servlet.ServletContext;
 
 public class EventServlet extends HttpServlet {
 
@@ -14,6 +15,31 @@ public class EventServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        DAOFactory daoFactory = null;
+        
+        ServletContext context = request.getServletContext();
+        
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating new DAOFactory ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        response.setContentType("application/json; charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            
+            EventDAO e_dao = daoFactory.getEventDAO();
+            out.println(e_dao.find(id));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     
